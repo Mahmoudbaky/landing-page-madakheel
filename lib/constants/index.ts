58f5e4e8855ab8a7c7204b1c778ced56247c31a2
MAHMOUD <ma7mouudbaky@gmail.com>
2025-01-25 15:28:42 +0300

    test

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,5 +1,5 @@
 export const APP_NAME_AR = "مداخيل";
-export const APP_NAME_EN = "maddakhel";
+export const APP_NAME_EN = "madakheel";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION || "company of maddakhel";
 export const SERVER_URL =
